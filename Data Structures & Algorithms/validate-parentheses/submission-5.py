2821976c02cc2a1155b085caa69cd9133251534a
Aryan Patil <apatil.connect@gmail.com>
2026-08-05 14:01:43 -0400

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.py
@@ -0,0 +1,37 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        """
+        Stack --> every time we encounter char in ['{','(','['] we push it on the stack
+        
+        ever time we encounter char in the complement we pop from the stack
+
+        return true if our stack isEmpty()
+
+        false otherwise
+        """
+        validStack = []
+        for char in s:
+            if char in ['{','(','[']:
+                validStack.append(char)
+            else:
+                if not validStack:
+                    return False
+
+                top = validStack[-1]
+                if top == '{':
+                    complement = '}'
+                elif top == '[':
+                    complement = ']'
+                elif top == '(':
+                    complement = ')'
+                
+                if char == complement:
+                    validStack.pop()
+                else:
+                    return False
+        
+        if not validStack:
+            return True
+        else:
+            return False
+            
